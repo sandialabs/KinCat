8ee6316155d7368bd16d6499b4613755dc1499b7
cjdanie <92339559+cjdanie@users.noreply.github.com>
2023-04-13 09:32:45 -0600

    Create KinCat.hpp

---
+++ src/core/KinCat.hpp
@@ -0,0 +1,17 @@
+#ifndef __KINCAT_HPP__
+#define __KINCAT_HPP__
+
+#include "KinCat_Util.hpp"
+
+#include "KinCat_Lattice.hpp"
+#include "KinCat_ProcessCounter.hpp"
+#include "KinCat_ProcessDictionary.hpp"
+
+#include "KinCat_SolverBase.hpp"
+#include "KinCat_SolverSerialRTA.hpp"
+#include "KinCat_SolverSublattice.hpp"
+
+#include "KinCat_Dump.hpp"
+#include "KinCat_Stats.hpp"
+
+#endif
