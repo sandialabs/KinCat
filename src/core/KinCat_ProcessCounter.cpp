137305f1a5ccf27a2ef26e081e1ac47f8e84fa0f
cjdanie <92339559+cjdanie@users.noreply.github.com>
2024-05-13 08:44:06 -0600

    Update KinCat_ProcessCounter.cpp

--- src/core/KinCat_ProcessCounter.cpp
+++ src/core/KinCat_ProcessCounter.cpp
@@ -3,18 +3,22 @@
 namespace KinCat {
 
 template <typename DT>
-ProcessCounter<DT>::ProcessCounter(const ordinal_type n_processes,
+
+ProcessCounter<DT>::ProcessCounter(const ordinal_type n_domains, const ordinal_type n_processes,
                                    const value_type_2d_view<ordinal_type, DT> &processints)
-    : _is_initialized(false), _processints(processints), _n_processes(n_processes), _process_counter(),
+    : _is_initialized(false), _processints(processints),_n_domains(n_domains), _n_processes(n_processes), _process_counter(),
       _processints_counter(), _process_counter_host(), _processints_counter_host() {}
 
-template <typename DT> void ProcessCounter<DT>::initialize(const ordinal_type n_samples) {
-  _process_counter = value_type_2d_view<size_type, DT>("process counter", n_samples, _n_processes);
-  _processints_counter =
-      value_type_2d_view<size_type, DT>("process instance counter", n_samples, _processints.extent(0) + 1);
 
+template <typename DT> void ProcessCounter<DT>::initialize(const ordinal_type n_samples) {
+  _process_counter = value_type_2d_view<size_type, DT>("process counter", n_samples, _n_processes*_n_domains);
+  _n_processints = _processints.extent(0);
+  _processints_counter =
+      value_type_2d_view<size_type, DT>("process instance counter", n_samples, (_processints.extent(0)*_n_domains) + 1);
   _process_counter_host = Kokkos::create_mirror_view(host_device_type(), _process_counter);
   _processints_counter_host = Kokkos::create_mirror_view(host_device_type(), _processints_counter);
+
+  //_functiontimer = value_type_1d_view<std::chrono::duration<double>, DT>("function timers", 5);
 
   _is_initialized = true;
 }
@@ -43,8 +47,15 @@ ProcessCounter<DT>::getProcessIntsCounterHost(const ordinal_type sid) const {
 }
 
 template <typename DT>
+ordinal_type
+ProcessCounter<DT>::getTotalProcessCount(const ordinal_type s) const {
+  return _processints_counter_host(s, _processints_counter_host.extent(1) - 1);
+}
+
+template <typename DT>
 std::ostream &ProcessCounter<DT>::showMe(std::ostream &os, const std::string &label, const ordinal_type verbose) const {
   const std::string indent2("  "), indent4("  "), indet6("      ");
+  //os << "n_domains in Counter : " << _n_domains << '\n';
   // os << "-- ProcessCounter : " << label << "\n";
   // os << indent2 << "-- is initialized : " << (_is_initialized ? "true" : "false") << "\n";
 
@@ -53,17 +64,27 @@ std::ostream &ProcessCounter<DT>::showMe(std::ostream &os, const std::string &la
     const ordinal_type n_samples = _process_counter_host.extent(0);
     for (ordinal_type s = 0, send = n_samples; s < send; ++s) {
       os << indent2
-         << "-- # of events occured : " << _processints_counter_host(s, _processints_counter_host.extent(1) - 1)
-         << "\n";
+         << "-- # of events occured : " << _processints_counter_host(s, _n_domains*_n_processints) << "\n"; //(s, _processints_counter_host.extent(1) - 1) ???
       os << indent2 << "-- sample id : " << s << "\n";
       os << indent2 << "-- process counter : \n";
-      for (ordinal_type i = 0, iend = _process_counter_host.extent(1); i < iend; ++i)
-        os << indent4 << "[ " << i << ", " << _process_counter_host(s, i) << "] \n";
+      for (ordinal_type i = 0, iend = _process_counter_host.extent(1); i < iend; ++i) {
+        ordinal_type tmp_counter = 0;
+        for (ordinal_type k = 0, kend = _n_domains; k < kend; ++k) {
+          tmp_counter += _processints_counter_host(s, i + (k * iend));
+        }
+        os << indent4 << "[ " << i << ", " << tmp_counter << "] \n";
+      }
 
       if (verbose > 3) {
-        os << indent2 << "-- process counter : \n";
-        for (ordinal_type i = 0, iend = (_processints_counter_host.extent(1) - 1); i < iend; ++i)
-          os << indent4 << "[ " << i << ", " << _processints_counter_host(s, i) << "] \n";
+        os << indent2 << "-- process instance counter : \n";
+        ordinal_type tmp_counter;
+        for (ordinal_type i = 0, iend = (_n_processints); i < iend; ++i) {
+          tmp_counter = 0;
+          for (ordinal_type k = 0, kend = _n_domains; k < kend; ++k) {
+            tmp_counter += _processints_counter_host(s, i + (k*iend));
+          }
+          os << indent4 << "[ " << i << ", " << tmp_counter << "] \n";
+        }
       }
     }
   }
@@ -82,12 +103,20 @@ std::ostream &ProcessCounter<DT>::showMe(std::ostream &os, const std::string &la
         os << indent2 << "-- sample id : " << s << "\n";
       }
       os << indent2
-         << "-- # of events occured : " << _processints_counter_host(s, _processints_counter_host.extent(1) - 1)
+         //<< "-- # of events occured : " << _processints_counter_host(s, _processints_counter_host.extent(1) - 1)
+         << "-- # of events occured : " << _processints_counter_host(s, _n_domains*_n_processints) //1 ???
          << "\n";
+      //std::cout << _processints_counter_host.extent(0) << ", " << _processints_counter_host.extent(1) << '\n';
       if (verbose > 3) {
-        os << indent2 << "-- process counter : \n";
-        for (ordinal_type i = 0, iend = (_processints_counter_host.extent(1) - 1); i < iend; ++i)
-          os << indent4 << "[ " << i << ", " << _processints_counter_host(s, i) << "] \n";
+        os << indent2 << "-- process instance counter : \n";
+        ordinal_type tmp_counter;
+        for (ordinal_type i = 0, iend = (_n_processints); i < iend; ++i) {
+          tmp_counter = 0;
+          for (ordinal_type j = 0, jend = _n_domains; j < jend; ++j) {
+            tmp_counter += _processints_counter_host(s, i+(j*iend));
+          }
+          os << indent4 << "[ " << i << ", " << tmp_counter << "] \n";
+        } 
       }
     }
   }
